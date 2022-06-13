e84832e7db5b4576fbf0eaafdb9e30c79d5ca7c2
M17ggie <102402171+M17ggie@users.noreply.github.com>
2022-06-13 22:13:48 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -2,16 +2,41 @@ import { useState } from "react";
 import OrderData from "./components/OrderData";
 import OrderDataForm from "./components/OrderDataForm";
 
+const orders = [{
+  key: Math.random().toString(),
+  orderId: 1,
+  name: 'Black Cat',
+  purchaseDate: '2022-12-06',
+  deliveryDate: '2022-11-05',
+  quantity: 5,
+  amount: 450
+},
+{
+  key: Math.random().toString(),
+  orderId: 2,
+  name: 'Maggie',
+  purchaseDate: '2022-15-06',
+  deliveryDate: '2022-10-05',
+  quantity: 10,
+  amount: 10000
+},
+{
+  key: Math.random().toString(),
+  orderId: 3,
+  name: 'Danny',
+  purchaseDate: '2022-15-06',
+  deliveryDate: '2022-17-05',
+  quantity: 50,
+  amount: 50000
+}]
+
 function App() {
 
-  const [data, setData]=useState()
+  const [data, setData] = useState(orders)
 
-  const getDataHandler=(enteredOrder)=>{
-    const newOrder={
-      ...enteredOrder
-    }
-    // console.log(newOrder)
-    setData(newOrder)
+  const getDataHandler = (enteredOrder) => {
+    
+    setData(prevOrder=>[...prevOrder, enteredOrder])
   }
 
   return (
